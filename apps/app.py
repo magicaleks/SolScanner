7a3d3dcb66865709620b16a51011579909e1902d
ZzzaLex <android1vvv@gmail.com>
2024-03-22 21:56:53 +1200

    fix

--- apps/app.py
+++ apps/app.py
@@ -2,7 +2,7 @@ from scanner.scanner import Scanner
 from telegram.client import TelegramBot
 
 # tg = TelegramBot("6795829087:AAGX-GvhUBpYa0IhFp2hNLk6sMol1i2KBDw", "-2135735087")
-tg = TelegramBot("6795829087:AAGX-GvhUBpYa0IhFp2hNLk6sMol1i2KBDw", "-4149457412")
+tg = TelegramBot("6795829087:AAGX-GvhUBpYa0IhFp2hNLk6sMol1i2KBDw", "-2013471410")
 # print(asyncio.run(tg.bot.get_chat("945482940")))
 
 Scanner(tg).run()
